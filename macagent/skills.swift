965dd4ca2a7df34356d890650f4c436dde00cf81
Petr Homola <phomola@me.com>
2026-04-06 16:38:37 +0100

    AppleScript common code (#10)

--- macagent/skills.swift
+++ macagent/skills.swift
@@ -11,3 +11,14 @@ struct SkillResult {
     let message: String
     let success: Bool
 }
+
+func run(appleScript script: String, successMessage: String) -> SkillResult {
+    guard let script = NSAppleScript(source: script) else { return SkillResult(message: "AppleScript couldn't be created.", success: false) }
+    var errorInfo: NSDictionary? = nil
+    script.executeAndReturnError(&errorInfo)
+    if errorInfo == nil {
+        return SkillResult(message: successMessage, success: true)
+    } else {
+        return SkillResult(message: errorInfo?[NSAppleScript.errorMessage] as? String ?? "Unknown AppleScript error.", success: false)
+    }
+}
